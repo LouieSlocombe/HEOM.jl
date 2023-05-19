4f8b4e4546e49064b1bb0f93ac60d5bf7a741e16
Louie <louies@hotmail.co.uk>
2023-05-19 18:50:09 +0100

    added free particle test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,6 @@ using Test, SafeTestsets
 
 begin
     @time @safetestset "Derivatives" begin include("derivatives.jl") end
-    #@time @safetestset "" begin include(".jl") end
+    @time @safetestset "Wigner Moyal Free Particle" begin include("core_eq/wigner_moyal_free.jl") end
+    @time @safetestset "LL HT M Free Particle" begin include("core_eq/wigner_moyal_free.jl") end
 end
