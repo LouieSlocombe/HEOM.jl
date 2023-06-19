38e005946cb2a68e19d58252612ca03e2793f8e0
Louie <louies@hotmail.co.uk>
2023-06-19 11:20:08 +0100

    fixed plots

--- src/phase_space_plots.jl
+++ src/phase_space_plots.jl
@@ -8,25 +8,34 @@ function os_display(p)
     end
 end
 
-function plot_wigner_heatmap(x, y, z; name="w_heatmap.pdf", title="")
-    xlab = latexstring("Q \\: \\left[\\alpha_0 \\right]")
-    ylab = latexstring("P \\: \\left[\\hbar / \\alpha_0 \\right]")
-    z = permutedims(z, (2, 1))
-    p = heatmap(
-        x,
-        y,
-        z,
+function plot_wigner_heatmap(
+    q,
+    p,
+    w;
+    name="w_heatmap.pdf",
+    title="",
+    xlab=latexstring("Q \\: \\left[\\alpha_0 \\right]"),
+    ylab=latexstring("P \\: \\left[\\hbar / \\alpha_0 \\right]")
+)
+    # Fix the axes
+    w = permutedims(w, (2, 1))
+    # Plot the heatmap
+    fig = heatmap(
+        q,
+        p,
+        w,
         seriescolor=:inferno,
         xlabel=xlab,
         ylabel=ylab,
         title=title,
-        xlims=(minimum(x), maximum(x)),
-        ylims=(minimum(y), maximum(y)),
+        xlims=(minimum(q), maximum(q)),
+        ylims=(minimum(p), maximum(p)),
         right_margin=10.0Plots.mm,
     )
-    savefig(p, joinpath(plot_dump, name))
-    os_display(p)
-    return p
+    # Save the figure
+    savefig(fig, joinpath(plot_dump, name))
+    os_display(fig)
+    return fig
 end
 
 function animate_wigner_heatmap(
@@ -35,7 +44,9 @@ function animate_wigner_heatmap(
     sol;
     time=nothing,
     name="ani_wigner_heatmap.gif",
-    title=""
+    title="",
+    xlab=latexstring("Q \\: \\left[\\alpha_0 \\right]")
+    ylab = latexstring("P \\: \\left[\\hbar / \\alpha_0 \\right]")
 )
     """
     Animate wigner function heatmap
@@ -52,11 +63,9 @@ function animate_wigner_heatmap(
     z_max = maximum(z_vals)
     z_min = minimum(z_vals)
 
-    xlab = latexstring("Q \\: \\left[\\alpha_0 \\right]")
-    ylab = latexstring("P \\: \\left[\\hbar / \\alpha_0 \\right]")
     @time begin
         anim = @animate for i = 1:nt
-            tmp = heatmap(
+            heatmap(
                 q,
                 p,
                 permutedims(z_vals[:, :, i], (2, 1)),
@@ -70,9 +79,9 @@ function animate_wigner_heatmap(
                 right_margin=10.0Plots.mm,
             )
         end
-        p = gif(anim, joinpath(plot_dump, name), fps=60)
+        fig = gif(anim, joinpath(plot_dump, name), fps=60)
     end
-    os_display(p)
-    return p
+    os_display(fig)
+    return fig
 end
 
